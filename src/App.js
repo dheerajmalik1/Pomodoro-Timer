d3d7ff8a283b9ab671ff5fb64b2b075b073415ad
Dheeraj Malik <37261043+dheerajmalik1@users.noreply.github.com>
2021-08-30 13:37:45 +0530

    fix:timer going in negative value

--- src/App.js
+++ src/App.js
@@ -9,28 +9,33 @@ export default function App() {
     .padStart(2, "0");
   const secondsLeft = (timeLeft - minutesLeft * 60).toString().padStart(2, "0");
   // let counter;
-  useEffect(() => {
-    console.log("from use effect", timeLeft);
-  });
+  // useEffect(() => {
+  //   console.log("from use effect", timeLeft);
+  // });
 
   function startPomodoro() {
     const inter = setInterval(function () {
-      console.log("inside set interval", timeLeft);
+      // console.log("inside set interval", timeLeft);
       //this value of set interval is not changing, why?
       //checking changes
-      if (timeLeft <= 0) {
-        console.log("will this ever work");
-      }
-      setTimeLeft((timeLeft) => timeLeft - 2);
+      // if (timeLeft <= 0) {
+      //   console.log("will this ever work");
+      // }
+      setTimeLeft((timeLeft) => {
+        if (timeLeft > 0) {
+          return timeLeft - 1;
+        }
+        return 0;
+      });
       // console.log(timeLeft);
     }, 1000);
   }
-  console.log("above return");
+  // console.log("above return");
 
   return (
     <div className="App">
       <div className="pomodoro-box">
-        <h2>Some dialogue here</h2>
+        <h2>Some</h2>
         <div className="timer">
           <span>{minutesLeft}</span>:<span>{secondsLeft}</span>
         </div>
